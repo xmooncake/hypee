a2e52c8f09bc4d529448dc112dccbc62e58eb5f6
xmooncake <s.gierlachpl@gmail.com>
2024-06-15 19:20:01 +0200

    Update FlutterError handling and localization labels

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -24,9 +24,9 @@ class AppBlocObserver extends BlocObserver {
 }
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
-  // FlutterError.onError = (details) {
-  //   log(details.exceptionAsString(), stackTrace: details.stack);
-  // };
+  FlutterError.onError = (details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
 
   Bloc.observer = const AppBlocObserver();
 
